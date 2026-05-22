2f5771461ab8308c51341ec5264e43d113c6dce7
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-05-22 09:24:28 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -11,18 +11,31 @@ int main(int argc, const char* argv[]) {
     int count = 0;
     Student students[64];
 
-
+    while (!feof(fp)) {
+        if (fscanf(fp, "%s %d", students[count].name, &students[count].score) == 2) {
+            count++;
+        }
+    }
 
     int max = 0;
     float avg = 0;
 
-
+    if (count > 0) {
+        int sum = 0;
+        max = students[0].score;
+        
+        for (int i = 0; i < count; i++) {
+            if (students[i].score > max) {
+                max = students[i].score;
+            }
+            sum += students[i].score;
+        }
+        avg = (float)sum / count;
+    }
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
 
     fclose(fp);
-
     return 0;
 }
-

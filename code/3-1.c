512383b2499bb633fe320f7e349101df831285dc
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-05-22 10:11:32 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -6,7 +6,7 @@ int main(int argc, const char* argv[]) {
     
     FILE* fp = fopen(argv[1], "r");
     if (fp == NULL) {
-        fprintf(stderr, "file does not exists");
+        peeor("file dose not exists");
         return 1;
     }
 

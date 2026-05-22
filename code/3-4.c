1d1a8ee5ad585443525d5699c99fe56f015981fc
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-05-22 09:52:18 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
+    if (argc < 3) {return 1;}
+    
+    FILE* fp_src = fopen(argv[1], "r");
+    if (fp_src == NULL) {
+        fprintf(stderr, "error: cannot open\n");
+        return 1;
+    }
+    FILE* fp_dst = fopen(argv[2], "w");
+    if (fp_dst == NULL) {
+        fprintf(stderr, "error: cannot open\n");
+        fclose(fp_src);
+        return 1;
+    }
 
-    "error: cannot open\n"
-    "error: cannot read\n"
-    "error: cannot write\n"
+    int c;
+    while ((c = fgetc(fp_src)) != EOF) {
+        if (fputc(c, fp_dst) == EOF) {
+            fprintf(stderr, "error: cannot write\n"); [cite: 524]
+            fclose(fp_src);
+            fclose(fp_dst);
+            return 1;
+        }
+    }
 
+    if (ferror(fp_src)) {
+        fprintf(stderr, "error: cannot read\n"); [cite: 523]
+        fclose(fp_src);
+        fclose(fp_dst);
+        return 1;
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
+    return 0;
 }
-

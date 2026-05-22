743c2e3865df47fd061bcb425a604db5c7be7685
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-05-22 09:54:40 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -2,15 +2,23 @@
 
 int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
-
+    
     FILE* fp = fopen(filename, "r");
     if (fp == NULL) {
         return 0;
     }
 
-
     int sum = 0;
-
+    char buffer[64];
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        int val;
+        if (sscanf(buffer, "%d", &val) == 1) {
+            sum += val;
+        } else {
+            buffer[char_caps - 1] = '\0'; 
+            fprintf(stderr, "invalid input %s", buffer); 
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
